f9d4077a6cf2249cf34b0192e6afebe43b5cfd92
Marcelo Carvalho <marcelo.santana@edu.sc.senai.br>
2024-02-22 14:47:44 -0300

    handling volume

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,7 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
- 
+const volumeSlider = document.querySelector(".volume-slider input");
+
+let mapedKeys = [];
 let audio = new Audio("src/tunes/a.wav");
 
 const playTune = (key) => {
@@ -16,9 +18,17 @@ const playTune = (key) => {
 
 pianoKeys.forEach((key) => {
   key.addEventListener("click", () => playTune(key.dataset.key));
+  mapedKeys.push(key.dataset.key);
 });
 
 document.addEventListener("keydown", (e) => {
-      playTune(e.key);
-
+    if (mapedKeys.includes(e.key)) {
+        playTune(e.key);
+      }
   });
+
+  const handleVolume = (e) => {
+    audio.volume = e.target.value;
+  };
+  
+  volumeSlider.addEventListener("input", handleVolume);
